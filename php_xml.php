6379a1b0353d5a929c0c0d572cd8f8295dc7d405
moahmed amine mezghich <git@mezghich.com>
2022-11-16 10:56:56 +0100

    lecture de data d'un fichier xml

--- php_xml.php
+++ php_xml.php
@@ -1,5 +1,10 @@
 <?php
 
 echo"<h1 align=center>PHP et les fichiers XML</h1>";
-
+$xml=simplexml_load_file("bib.xml");
+//var_dump($xml);
+// Lecture des données du premier element du fichier xml
+echo "Titre :".$xml->titre."<br />";
+echo "Auteur :".$xml->auteur."<br />";
+echo "Date :".$xml->date."<br />";
 ?>
